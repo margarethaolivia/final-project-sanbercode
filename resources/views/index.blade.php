9c3d8661d3c540c57d50fe3b298ec4a55446dcb3
kevinsrrno <67588306+kevinsrrno@users.noreply.github.com>
2020-07-09 10:19:49 +0700

    edit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -76,7 +76,7 @@ integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6J
     </ul>
     <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit"  onkeyup="myFunction()>Search</button>
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit" >Search</button>
     </form>
   </div>
 </nav>
