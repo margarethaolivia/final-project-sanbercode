3d3df5c63b9fcdf0decc6b0ced3265de9d8565ac
Margaretha Olivia <margarethaolivia41@gmail.com>
2020-07-11 18:41:48 +0700

    menambahkan ERD

--- resources/views/index1.blade.php
+++ resources/views/index1.blade.php
@@ -15,13 +15,13 @@
 <div class="mySlides fade">
   <div class="numbertext">2 / 3</div>
   <img src="{{asset('img/Screenshot (95).png')}}" style="width:100%">
-  <div class="text">Caption Two</div>
+  <div class="text"></div>
 </div>
 
 <div class="mySlides fade">
   <div class="numbertext">3 / 3</div>
   <img src="{{asset('img/Screenshot (96).png')}}" style="width:100%">
-  <div class="text">Caption Three</div>
+  <div class="text"></div>
 </div>
 
 <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
